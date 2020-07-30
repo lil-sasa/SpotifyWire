09d2a0945c9a03cb8cfd146f80fbe65dc25e6c14
Sasaoka <Sasaoka>
2020-07-30 02:41:17 -0300

    trocado helper asset para secure_asset para funcionar com protocolo https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,9 @@
 
         
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
-        <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/x-icon" />
+        <link rel="icon" href="{{ secure_asset('img/logo.png') }}" type="image/x-icon" />
 
         <livewire:styles>
 
@@ -22,6 +22,6 @@
         <livewire:scripts>
 
         @stack('scripts')
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ secure_asset('js/app.js') }}"></script>
     </body>
 </html>
--- resources/views/livewire/pesquisa.blade.php
+++ resources/views/livewire/pesquisa.blade.php
@@ -1,7 +1,7 @@
 <div class="relative">
 
     <div class="flex justify-center mt-5">
-        <img src="{{ asset('img/spotifyWire.png') }}">
+        <img src="{{ secure_asset('img/spotifyWire.png') }}">
     </div>
     {{-- Barra de pesquisa --}}
     <div class="p-10 flex flex-wrap sys-app-notCollapsed justify-center"> 
@@ -9,7 +9,7 @@
             <div class="p-2 text-gray-900 bg-white rounded-lg shadow-lg w-full">
     
                 <span class="px-2 mr-2 border-r border-gray-800">
-                    <img src="{{ asset('img/logo.png') }}"
+                    <img src="{{ secure_asset('img/logo.png') }}"
                         alt="alt placeholder" class="w-8 h-8 -mt-1 inline mx-auto">
                 </span>
                 <span class="px-1 cursor-pointer hover:text-gray-700">
