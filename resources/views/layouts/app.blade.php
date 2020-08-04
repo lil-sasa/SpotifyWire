9e2136b9eff84ed915821ce9e05d7a147f8a4ca2
Sasaoka <Sasaoka>
2020-08-04 16:11:08 -0300

    setado para produção

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,15 +8,15 @@
 
         
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
-        <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/x-icon" />
+        <link rel="icon" href="{{ secure_asset('img/logo.png') }}" type="image/x-icon" />
 
         <livewire:styles>
 
         <livewire:scripts>
 
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ secure_asset('js/app.js') }}"></script>
         @stack('scripts')
 
     </head>
